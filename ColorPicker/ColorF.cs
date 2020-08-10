4ea2661387e312a1245c694e7fd3b7a88341be9e
Iori Komatsu <iori.komatsu@gmail.com>
2020-08-10 18:07:51 +0900

    使ってないメソッドを削除

--- ColorPicker/ColorF.cs
+++ ColorPicker/ColorF.cs
@@ -27,38 +27,6 @@ namespace ColorPicker {
             return new ColorF(r, g, b);
         }
 
-        public (float, float, float) ToHSV() {
-            float max = Math.Max(R, Math.Max(G, B));
-            float min = Math.Min(R, Math.Min(G, B));
-            float delta = max - min;
-
-            float h;
-            if (delta < 1e-10) {
-                h = 0;
-            } else if (max == R) {
-                h = (((G - B) / delta) % 6.0f) / 6.0f;
-            } else if (max == G) {
-                h = (((B - R) / delta) + 2.0f) / 6.0f;
-            } else {
-                h = (((R - G) / delta) + 4.0f) / 6.0f;
-            }
-
-            float s = max < 1e-10 ? 0 : delta / max;
-
-            float v = max;
-
-            return (h, s, v);
-        }
-
-        private static float ToSRGB1(float x) {
-            float x_ = Math.Max(6.10352e-5f, x);
-            return Math.Min(x_ * 12.92f, (float)Math.Pow(Math.Max(x_, 0.00313067f), 1.0 / 2.4) * 1.055f - 0.055f);
-        }
-
-        public ColorF ToSRGB() {
-            return new ColorF(ToSRGB1(A), ToSRGB1(R), ToSRGB1(G), ToSRGB1(B));
-        }
-
         private static byte FloatToByte(float x) {
             int i = (int)(x * 256.0f);
             if (i < 0) return 0;
